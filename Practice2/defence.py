3f913ca144da4975f816c14f3c98848b5574add1
AbdrakhmanTurdymurat <turdymurat07@inbox.ru>
2026-02-06 13:50:11 +0500

    defence work

--- Practice2/defence.py
+++ Practice2/defence.py
@@ -1,35 +1,17 @@
-students = {
-    "Али": 85,
-    "Айша": 92,
-    "Нурлан": 48,
-    "Дана": 78,
-    "Ержан": 95
+name = {
+    "Nurlan": 21,
+    "Bekslan": 40, 
+    "Ainur": 18,
+    "Aibek": 16,
 }
+allowed = {"Nurlan", "Ainur"}
 
-def get_gpa(score):
-    if score >= 95:
-        return 4.0
-    elif score >= 90:
-        return 3.7
-    elif score >= 85:
-        return 3.3
-    elif score >= 80:
-        return 3.0
-    elif score >= 75:
-        return 2.7
-    elif score >= 70:
-        return 2.3
-    elif score >= 65:
-        return 2.0
-    elif score >= 60:
-        return 1.7
-    elif score >= 55:
-        return 1.3
-    elif score >= 50:
-        return 1.0
+def checker(score):
+    if score >= 18:
+        return True
     else:
-        return 0.0
-
-for name, score in students.items():
-    gpa = get_gpa(score)
-    print(f"{name}: {score} -> GPA {gpa}")
+        return False
+    
+for name,score in name.items():
+    if checker and name in allowed :
+        print(f"{name}, {score} allowed")
